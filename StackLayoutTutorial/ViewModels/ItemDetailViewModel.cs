e3e98f86fd21a2dbdcc1cd293c4d259916fef4ab
Влад Цуканов <t5ukanov.v@yandex.ru>
2021-12-07 22:41:06 +0300

    StackLayoutTutuorial

---
+++ StackLayoutTutorial/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using StackLayoutTutorial.Models;
+
+namespace StackLayoutTutorial.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
